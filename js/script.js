fe32c682f7a0ed3266ccbcd1dcbb7c63dd828b57
Frederick Silva <fred@thehouseofsilva.com>
2016-09-29 17:50:39 -0300

    news styles

--- js/script.js
+++ js/script.js
@@ -1,46 +1,4 @@
 ;(function() {
-
-  var words = [
-    'front-end',
-    'código criativo',
-    'open-source lover',
-    'comunidade',
-    'aprendizado contínuo',
-    'liberdade',
-    'responsabilidade',
-    'crocante',
-    'cremosa',
-    'acessível',
-    'possível',
-    'realista',
-    'organizada',
-    'inclusiva'
-  ];
-  
-  var interval;
-  var clicked = false;
-  var qtyWords = words.length - 1;
-
-  interval = setInterval( changeWord, 125 );
-
-  $( '#main #grama-is' ).on( 'click', function() {
-    if ( clicked ) {
-      clicked = false;
-      interval = setInterval( changeWord, 125 );
-    } else {
-      clicked = true;
-      clearInterval( interval );
-    }
-  });
-
-  function changeWord() {
-    $( '.grama-word' ).text( words[qtyWords] );
-
-    if ( qtyWords ) {
-      qtyWords--;
-    } else {
-      qtyWords = words.length - 1;
-    }
-  }
+ console.log("LAB.Rio é Governo Aberto")
 
 })();
